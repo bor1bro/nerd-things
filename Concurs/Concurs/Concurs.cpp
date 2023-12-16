3193dff18b4613c3ed8560aeb4f68e09145efae8
bor1bro <133193063+bor1bro@users.noreply.github.com>
2023-12-16 18:08:29 +0300

    Start
    everything is so bad

---
+++ Concurs/Concurs/Concurs.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <sstream>
+#include <fstream>
+#define filename "kids_wish.csv"
+using namespace std;
+int main()
+{
+
+    ifstream work_file(filename);
+    unsigned short int GiftCounter[1000];
+    string line;
+    char delimiter = ',';
+    while (getline(work_file, line))
+    {
+        int Counter = 0;
+        stringstream stream(line);
+        string idGift;
+        for (int Index = 0; Index < 100; Index++)
+        {
+            getline(stream, idGift, delimiter);
+            cout << idGift << " ";
+            Counter++;
+        }
+        cout << endl;
+        cout << endl;
+        cout << Counter << endl;
+        cout << endl;
+        cout << endl;
+    }
+    work_file.close();
+}
