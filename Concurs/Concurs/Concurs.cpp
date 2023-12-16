de16eb7bb037dbfa46ffe456eb33d6905c9fa8ce
bor1bro <133193063+bor1bro@users.noreply.github.com>
2023-12-16 20:37:32 +0300

    Sorta working state
    CSV IS READEN AND WRITTEN BABYYYYYYY

--- Concurs/Concurs/Concurs.cpp
+++ Concurs/Concurs/Concurs.cpp
@@ -1,31 +1,78 @@
 ﻿#include <iostream>
 #include <sstream>
 #include <fstream>
-#define filename "kids_wish.csv"
+#include <vector>
+#define kids "kids_wish.csv"
+#define ded "ded_moroz_wish.csv"
+#define result "result.csv"
+#define size 10
 using namespace std;
-int main()
-{
 
-    ifstream work_file(filename);
-    unsigned short int GiftCounter[1000];
-    string line;
+int ANICH()
+{
+    return 0;
+}
+
+int ANIFH()
+{
+    return 0;
+}
+
+int ANIH()
+{
+    return ANICH() + ANIFH();
+}
+
+void TempIn(string line)
+{
+    ifstream work_file_kids(kids);
+    ofstream work_file_result(result);
+    char limiter = ',';
     char delimiter = ',';
-    while (getline(work_file, line))
+    vector<int> childGift;
+    int IntIdGift = 0;
+    int IntIdChild = 0;
+    while (getline(work_file_kids, line)) 
     {
-        int Counter = 0;
         stringstream stream(line);
         string idGift;
+        string idChild;
+        bool cycling = true;
+        getline(stream, idChild, delimiter);
         for (int Index = 0; Index < 100; Index++)
         {
             getline(stream, idGift, delimiter);
-            cout << idGift << " ";
-            Counter++;
+            IntIdGift = stoi(idGift);
+            IntIdChild = stoi(idChild);
+            childGift.push_back(IntIdGift);
         }
-        cout << endl;
-        cout << endl;
-        cout << Counter << endl;
-        cout << endl;
-        cout << endl;
-    }
-    work_file.close();
+        while (cycling)
+        {
+            int random_gift = rand() % 100;
+            for (int Index = 0; Index < 100; Index++)
+            {
+                if (childGift[Index] == random_gift)
+                {
+                    work_file_result << IntIdChild << limiter << random_gift << "\n";
+                    cout << "JOPA";
+                    cycling = false;
+                }
+            }
+        }
+        childGift.clear();
+    }   
+    work_file_kids.close();
+}
+
+int main()
+{
+    unsigned short int GiftCounter[1000];
+    //string FileName = kids;
+    string Line;
+    srand(time(0));
+
+    TempIn(Line);
+    ANIH();
+
+    
 }
